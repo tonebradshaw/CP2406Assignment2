f161247fff91be58e8fbe39661bdc3d7d76797d3
tonebradshaw <anthony.bradshaw@my.jcu.edu.au>
2016-10-14 17:18:15 +1000

    Test the GameDisplay class

---
+++ TestGameDisplay.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+/**
+ * Created by tony on 7/10/2016.
+ */
+public class TestGameDisplay {
+
+    public TestGameDisplay(){
+    }
+
+    public static void main(String[] args) {
+
+        GameDisplay game = new GameDisplay();
+        game.setVisible(true);
+    }
+}
