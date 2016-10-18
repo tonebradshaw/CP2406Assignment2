33a5b321933ac68dcb15100cce81188f8eab1df9
tonebradshaw <anthony.bradshaw@my.jcu.edu.au>
2016-10-18 17:48:26 +1000

    Change background colour to dark green

--- DisplayHand.java
+++ DisplayHand.java
@@ -12,28 +12,31 @@ public class DisplayHand extends JPanel {
 
     private GridLayout grid;
     private GridLayout grid1;
-    private PickUpCard pickUp;
+    private Color color;
 
-    public DisplayHand(ArrayList<Card> handCards, Card pickUpCard){
+
+    public DisplayHand(ArrayList<Card> handCards){
 
         Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();//get screen size
         int width = (int)screen.getWidth();
         int height = (int)screen.getHeight();
         grid = new GridLayout(2, 10); //2 rows
         grid1 = new GridLayout(1, 10); //1 row
+        color = new Color(0,153,76);
 
-        setBackground(Color.GREEN);
+        setBackground(color);
         setSize(width, height / 2);
+        System.out.println(handCards);
 
         for(int i = 0; i < handCards.size(); ++i) { //add card JButtons to DisplayHand JPanel
 
             add(handCards.get(i).button);
         }
-        pickUp = new PickUpCard();
-        add(pickUp.getButton());
+        //pickUp = new PickUpCard();
+        //add(pickUp.getButton());
 
 
-        if(handCards.size() <= 9){ //1 row if less than 10 cards else 2 rows
+        if(handCards.size() <= 8){ //1 row if less than 10 cards else 2 rows
             setLayout(grid1);
         }else{
             setLayout(grid);
