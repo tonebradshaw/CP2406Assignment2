1746e27e6aa461b1350945a7a64cf3bce0b9d294
tonebradshaw <anthony.bradshaw@my.jcu.edu.au>
2016-10-22 12:48:09 +1000

    Set JButton size

--- Card.java
+++ Card.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * Created by tony on 9/08/2016.
@@ -8,14 +9,21 @@ public abstract class Card {
     protected String name;
     protected JButton button;
     protected ImageIcon image;
+    protected ImageIcon backImage;
+
 
     public Card(String name, JButton button, ImageIcon image){ //card constructor
 
         this.name = name;
         this.button = button;
         this.image = image;
+        backImage = new ImageIcon("src\\CardImages\\PickUp.png");
+
+        Dimension dim = new Dimension(148,206);
+        button.setPreferredSize(dim);
     }
     public abstract String getName();
     public JButton getButton(){return button;}
     public ImageIcon getImage(){return image;}
+    public ImageIcon getBackImage(){return backImage;}
 }
