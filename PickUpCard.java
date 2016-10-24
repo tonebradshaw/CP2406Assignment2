0f96538cc01b9f6b0467d285faca18f7dcb13be4
tonebradshaw <anthony.bradshaw@my.jcu.edu.au>
2016-10-24 23:14:32 +1000

    Stop crash if players purposely run out of pickup cards

--- PickUpCard.java
+++ PickUpCard.java
@@ -23,23 +23,28 @@ public class PickUpCard {
             @Override
             public void actionPerformed(ActionEvent e) {
 
-                if(Play.shuffledDeck.size() == 0) {
-                    Play.checkPile();
+                if(Play.shuffledDeck.size() == 0) { //move discard pile to shuffled deck
+
+                    if(Play.discardedCards.size() == 0){
+
+                        JOptionPane.showMessageDialog(null, "<html>There are no more cards left to pick up<br>" +
+                                "As no-one has emptied their hand<br>the game is drawn</html");
+                        System.exit(0);
+                    } else {
+                        Play.checkPile();
+                    }
                 }
                 selectedCard = Play.shuffledDeck.get(0); //if card picked up
-
-                System.out.println("pickup " + Play.shuffledDeck.size());
                 GameDisplay.players[GameDisplay.playerNumber].getHand().add(selectedCard); //add card to hand
+                GameDisplay.players[GameDisplay.playerNumber].setPickUpCard(1);
                 Play.shuffledDeck.remove(0); //remove card from pickup deck
                 pickUp.setVisible(false);
                 GameDisplay.nextPlayer();
             }
-
         });
     }
     public JButton getButton(){
 
         return pickUp;
     }
-
 }
